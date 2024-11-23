39960084290f14ed0762d71f412571767d6fab4e
cyberdrek01 <144948926+Cyberdrek01@users.noreply.github.com>
2024-11-23 09:24:45 +0530

    ShoppingListApp

---
+++ BankAccountProgram/app/src/main/java/eu/tutorialls/bankaccountprogram/BankAccount.kt
@@ -0,0 +1,28 @@
+package eu.tutorialls.bankaccountprogram
+
+class BankAccount (var accountHolder:String,
+                   var balance: Double) {
+    private val transactionHistory= mutableListOf<String>()
+
+    fun deposit(amount:Double){
+        balance +=amount
+        transactionHistory.add("$accountHolder deposited Rs. $amount")
+    }
+
+    fun withdraw(amount:Double){
+        if(amount<=balance){
+            balance-=amount
+            transactionHistory.add("$accountHolder withdrew Rs $amount")
+        }
+        else{
+            println("You don't have the funds to withdraw Rs. $amount")
+        }
+    }
+
+    fun displayTransactionHistory(){
+        println("Transaction history for $accountHolder")
+        for(transaction in transactionHistory){
+            println(transaction)
+        }
+    }
+}
--- day4/app/src/main/java/eu/tutorialls/day4/basic.kt
+++ day4/app/src/main/java/eu/tutorialls/day4/basic.kt
@@ -1,11 +1,18 @@
 package eu.tutorialls.day4
 //67
-fun main(){
-    val shoppingList= mutableListOf("Graphics card","RAM","SSD","Monitor")
-    for (item in shoppingList){
-        print(item)
-        if(item=="RAM"){
-            break
-        }
+//fun main(){
+//    val shoppingList= mutableListOf("Graphics card","RAM","SSD","Monitor")
+//    println(shoppingList.size)
+//    for (index in 0 until shoppingList.size){
+//        println("item ${shoppingList[index]} is at index $index ")
+//
+//    }
+//}
+
+fun main() {
+    val numbers=mutableListOf(1,2,3,4,5)
+    for(index in 0 until numbers.size){
+        numbers[index]=numbers[index]*2
+        println(numbers[index])
     }
 }
